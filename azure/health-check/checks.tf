845a61c24e046384da4f216982a85696c6880d79
Mack Hendricks <mack@dopensource.com>
2023-12-06 14:34:31 -0500

    Fixed checks

--- azure/health-check/checks.tf
+++ azure/health-check/checks.tf
@@ -1,29 +1,29 @@
 #VM is UP Check
 
-data "azurerm_linux_virtual_machine" "webserver" {
-  name                = azurerm_linux_virtual_machine.webserver.*.name
+data "azurerm_virtual_machine" "webserver" {
+  name                = azurerm_virtual_machine.webserver.*.name
   resource_group_name = azurerm_resource_group.main.name 
 }
 
-check "check_vm_state" {
-  assert {
-    condition = data.azurerm_linux_virtual_machine.webserver.*.power_state == "running"
-    error_message = format("Virtual Machine (%s) should be in a 'running' status, instead state is '%s'",
-      data.azurerm_linux_virtual_machine.webserver.*.id,
-      data.azurerm_linux_virtual_machine.webserver.*.power_state
-    )
-  }
-}
+#check "check_vm_state" {
+#  assert {
+#    condition = data.azurerm_linux_virtual_machine.webserver.*.power_state == "running"
+#    error_message = format("Virtual Machine (%s) should be in a 'running' status, instead state is '%s'",
+#      data.azurerm_linux_virtual_machine.webserver.*.id,
+#      data.azurerm_linux_virtual_machine.webserver.*.power_state
+#    )
+#  }
+#}
 
 #HTTP Server is up
 check "health_check" {
   data "http" "webserver" {
-    url = "http://${data.azurerm_linux_virtual_machine.webserver.*.public_ip_address}"
+    url = "http://${data.azurerm_virtual_machine.webserver.*.public_ip_address}"
   }
 
   assert {
     condition = data.http.webserver.status_code == 200
-    error_message = "${data.azurerm_linux_virtual_machine.webserver.url} returned an unhealthy status code"
+    error_message = "${data.azurerm_virtual_machine.webserver.*.url} returned an unhealthy status code"
   }
 }
 
